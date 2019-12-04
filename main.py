c0603b3c7eae4c2e1e8d57cc6d22c072166eb25e
sunshineplan <sunshineplan@users.noreply.github.com>
2019-12-04 14:13:46 +0800

    Fix click run in debug option

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def backup():
 
 @cli.command(short_help='Run Server')
 @click.option('--port', '-p', default=80, help='Listening Port')
-@click.option('--debug', is_flag=False, hidden=True)
+@click.option('--debug', is_flag=True, hidden=True)
 def run(port, debug):
     if debug:
         app.run(host='0.0.0.0', port=port, debug=debug)
